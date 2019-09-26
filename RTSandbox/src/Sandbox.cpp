f328b62b0a79782ff2c12e7aebc702fa54b212d9
HaDriel <gautierlathu@hotmail.fr>
2019-09-27 00:16:27 +0200

    Adding the bunny to the project

--- RTSandbox/src/Sandbox.cpp
+++ RTSandbox/src/Sandbox.cpp
@@ -22,6 +22,11 @@ const int bl_sample_count = 100;
 
 void render_love()
 {
+    Mesh bunny;
+    bunny.load_off("bunny.off");
+    Transform bunnyTransform;
+    bunnyTransform.scale = {3000.f, 3000.f, 3000.f };
+
     Mesh love;
     love.load_obj("Love.obj");
     Transform loveTransform;
@@ -29,7 +34,6 @@ void render_love()
 
     Mesh cube;
     cube.load_obj("cube.obj");
-//    cube.load_off("cube.off"); // Working fine! 
     Transform cubeTransform;
     cubeTransform.translation = {0.f, -200.f, 0.f};
     cubeTransform.scale = {100.f, 100.f, 100.f};
@@ -41,9 +45,10 @@ void render_love()
     //Floor
     scene.createSphere(glm::vec3{0, -1e5, 0}, 1e5 - 300, glm::vec3{0.5f, 0.5f, 0.5f});
 
-    //Love
-    scene.createInstance(love, loveTransform);
-    scene.createInstance(cube, cubeTransform);
+    //Meshes
+    scene.createInstance(bunny, bunnyTransform);
+    // scene.createInstance(love, loveTransform);
+    // scene.createInstance(cube, cubeTransform);
 
     //Light
     // scene.createCubeLight(glm::vec3{0, 2000, 0}, 200, 2e6, glm::vec3{1.f, 1.f, 1.f});
@@ -54,17 +59,15 @@ void render_love()
     camera.fov = 60.f;
 
     Framebuffer framebuffer = Framebuffer(width, height);
-    int frameCount = 60;
     glm::quat orbit_rotation = glm::eulerAngleY(360.f / frameCount);
     for (int i = 0; i < frameCount; i++)
     {
-        std::cout << "Frame " << i << "/" << frameCount << " ";
+        std::cout << "Frame " << i << "/" << frameCount;
 
         //rotate around orbit
         camera.position = orbit_rotation * camera.position;
-
-        //Always look at the point of interest
-        camera.orientation = glm::quatLookAt(glm::normalize(camera.position), glm::vec3{0, 1, 0});
+        glm::vec3 direction = glm::normalize( glm::vec3{0, 100, 0}  - camera.position );
+        camera.rotation = glm::eulerAngles(glm::quatLookAt(direction, glm::vec3{0, 1, 0}));
 
         //Render to the framebuffer
         framebuffer.clear();
